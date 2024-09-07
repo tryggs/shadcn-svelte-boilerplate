0d1d129145ad227209229c69f5b13bfd6ed49e2a
Sebastian Trygg <sebbe.cqre@gmail.com>
2024-09-07 23:06:48 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<main class="flex h-screen w-full flex-col items-center justify-center gap-3 text-center">
+	<h1 class="text-5xl font-black">Shadcn-svelte Boilerplate</h1>
+	<h1 class="text-2xl font-medium text-foreground/75">Sebastian Trygg</h1>
+	<Button>Button hahahahha</Button>
+</main>
